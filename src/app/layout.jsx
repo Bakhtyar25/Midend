7d3e34dae659cd0c890f8a1a11002b7eabafda3f
Omed Akram <omedakram54@gmail.com>
2023-01-20 19:38:17 +0300

    layout Navbar modifi

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,14 +1,21 @@
 import '../styles/globals.css'
 import Nav from '@/components/nav'
 import Footer from '@/components/footer'
+import { Poppins } from '@next/font/google'
 
+
+
+const poppins = Poppins({
+  weight: '400',
+  subsets: ['latin'],
+})
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head />
 
-      <body>
+      <body className={poppins.className}>
 
         <div className='h-screen w-full bg-slate-400'>
         <Nav/>
